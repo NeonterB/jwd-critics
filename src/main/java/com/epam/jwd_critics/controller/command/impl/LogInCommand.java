76973598504d9f7a31d1c6f69c04dedf0b1f4233
Neonter <barkan.makar@gmail.com>
2021-06-06 00:18:53 +0300

    /

--- src/main/java/com/epam/jwd_critics/controller/command/impl/LogInCommand.java
+++ src/main/java/com/epam/jwd_critics/controller/command/impl/LogInCommand.java
@@ -22,51 +22,6 @@ public class LogInCommand implements Command {
                 return ServletDestination.MAIN_PAGE;
             }
         };
-        String login = (String) req.getAttribute(Attribute.LOGIN);
-        String password = (String) req.getAttribute(Attribute.PASSWORD);
-        if (login == null || password == null) {
-            sessionRequestContent.setSessionAttribute(Attribute.ERROR_MESSAGE, ErrorMessage.EMPTY_LOGIN_USER_PARAMETERS);
-            commandResult.setPage(PagePath.LOGIN);
-        } else {
-            Map.Entry<List<String>, List<String>> validationResult = userService.validateLoginData(login, password);
-            List<String> validParameters = validationResult.getKey();
-            List<String> errorMessages = validationResult.getValue();
-            if (!errorMessages.isEmpty()) {
-                sessionRequestContent.setSessionAttribute(Attribute.VALIDATION_ERRORS, errorMessages);
-                if (validParameters.contains(Attribute.LOGIN)) sessionRequestContent.setSessionAttribute(
-                        Attribute.LOGIN, login);
-                commandResult.setPage(PagePath.LOGIN);
-            } else {
-                User currentUser = (User) sessionRequestContent.getSessionAttribute(Attribute.USER);
-                if (currentUser.getId() != 0) {
-                    sessionRequestContent.setSessionAttribute(Attribute.ERROR_MESSAGE, ErrorMessage.ALREADY_LOGGED_IN);
-                    commandResult.setPage(PagePath.MAIN);
-                } else {
-                    Optional<String> errorMessage;
-                    Optional<User> user;
-                    Map.Entry<Optional<User>, Optional<String>> findResult;
-                    try {
-                        findResult = userService.login(login, password);
-                        user = findResult.getKey();
-                        errorMessage = findResult.getValue();
-                        if (errorMessage.isPresent()) {
-                            sessionRequestContent.setSessionAttribute(Attribute.ERROR_MESSAGE, errorMessage.get());
-                            sessionRequestContent.setSessionAttribute(Attribute.LOGIN, login);
-                            commandResult.setPage(PagePath.LOGIN);
-                        } else {
-                            if (user.isPresent()) {
-                                sessionRequestContent.setSessionAttribute(Attribute.USER, user.get());
-                                sessionRequestContent.setSessionAttribute(Attribute.SOME_USER, user.get());
-                                commandResult.setPage(PagePath.USER);
-                            }
-                        }
-                    } catch (ServiceException e) {
-                        logger.error(e);
-                        throw new CommandException(e);
-                    }
-                }
-            }
-        }
         return response;
     }
 
