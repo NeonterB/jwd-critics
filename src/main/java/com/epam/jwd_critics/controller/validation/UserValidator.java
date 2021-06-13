fdf3c531f26c2f23a6c2b5a22fcff9fabb176d7a
Neonter <barkan.makar@gmail.com>
2021-06-13 15:16:41 +0300

    new email regex

--- src/main/java/com/epam/jwd_critics/controller/validation/UserValidator.java
+++ src/main/java/com/epam/jwd_critics/controller/validation/UserValidator.java
@@ -9,7 +9,10 @@ import java.util.Set;
 public class UserValidator {
     private static final String FIRST_NAME_REGEX = "^[A-Z][a-z]{1,14}";
     private static final String LAST_NAME_REGEX = "^[A-Z][a-z]{1,14}";
-    private static final String EMAIL_REGEX = "^[A-Z][a-z]{1,14}";
+    private static final String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"" +
+            "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@" +
+            "(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}" +
+            "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
     private static final String LOGIN_REGEX = "^[a-zA-Z0-9._-]{3,25}$";
     private static final String PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$";
 
@@ -66,7 +69,7 @@ public class UserValidator {
         return violations;
     }
 
-    public Set<ConstraintViolation> validateRegisterData(String firstName, String lastName, String email, String login, String password) {
+    public Set<ConstraintViolation> validateRegistrationData(String firstName, String lastName, String email, String login, String password) {
         Set<ConstraintViolation> violations = new HashSet<>();
         validateFirstName(firstName).ifPresent(violations::add);
         validateLastName(lastName).ifPresent(violations::add);
