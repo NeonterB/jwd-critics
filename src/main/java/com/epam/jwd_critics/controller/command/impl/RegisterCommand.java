bf1a7b376b509ed7d198fdc10a66ac8555519f43
Neonter <barkan.makar@gmail.com>
2021-06-17 22:54:21 +0300

    Registration error handling

--- src/main/java/com/epam/jwd_critics/controller/command/impl/RegisterCommand.java
+++ src/main/java/com/epam/jwd_critics/controller/command/impl/RegisterCommand.java
@@ -16,40 +16,44 @@ import com.epam.jwd_critics.model.service.impl.UserServiceImpl;
 
 import javax.servlet.http.HttpSession;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class RegisterCommand implements Command {
     private final UserService userService = UserServiceImpl.getInstance();
 
     @Override
     public CommandResponse execute(CommandRequest req) {
-        CommandResponse response = new CommandResponse(ServletDestination.LOGIN_PAGE, TransferType.REDIRECT);
+        CommandResponse response = new CommandResponse(ServletDestination.SIGN_IN_PAGE, TransferType.REDIRECT);
         String firstName = req.getParameter(Parameter.FIRST_NAME);
         String lastName = req.getParameter(Parameter.LAST_NAME);
         String email = req.getParameter(Parameter.EMAIL);
         String login = req.getParameter(Parameter.LOGIN);
         String password = req.getParameter(Parameter.PASSWORD);
-        if (firstName == null || firstName.equals("") ||
-                lastName == null || lastName.equals("") ||
-                email == null || email.equals("") ||
-                login == null || login.equals("") ||
-                password== null || password.equals("")) {
-            //todo something bad
+        if (firstName == null ||
+                lastName == null ||
+                email == null ||
+                login == null ||
+                password == null) {
+            req.getSession(true).setAttribute(Attribute.VALIDATION_ERRORS.getName(), "Registration fields can't be empty");
         } else {
             UserValidator userValidator = new UserValidator();
             Set<ConstraintViolation> violations = userValidator.validateRegistrationData(firstName, lastName, email, login, password);
             if (violations.isEmpty()) {
                 try {
-                    User user = userService.register(firstName, lastName, email, login, password.toCharArray()
-                    );
+                    User user = userService.register(firstName, lastName, email, login, password.toCharArray());
                     HttpSession reqSession = req.getSession(true);
                     reqSession.setAttribute(Attribute.USER_ID.getName(), user.getId());
                     reqSession.setAttribute(Attribute.USER_ROLE.getName(), user.getRole());
                     response.setDestination(ServletDestination.MAIN_PAGE);
                     response.setTransferType(TransferType.REDIRECT);
                 } catch (ServiceException e) {
+                    req.getSession(true).setAttribute(Attribute.SERVICE_ERROR.getName(), e.getMessage());
                     return response;
                 }
             } else {
+                req.getSession(true).setAttribute(Attribute.VALIDATION_ERRORS.getName(), violations.stream()
+                        .map(ConstraintViolation::getMessage)
+                        .collect(Collectors.toList()));
                 return response;
             }
         }
