0f118e0248527f8fe25dda07af635a8ebd014d8c
Neonter <barkan.makar@gmail.com>
2021-06-19 14:52:37 +0300

    CommandException

--- src/main/java/com/epam/jwd_critics/controller/command/Command.java
+++ src/main/java/com/epam/jwd_critics/controller/command/Command.java
@@ -1,10 +1,12 @@
 package com.epam.jwd_critics.controller.command;
 
+import com.epam.jwd_critics.exception.CommandException;
+
 @FunctionalInterface
 public interface Command {
     static Command of(String commandName) {
         return CommandInstance.commandOf(commandName);
     }
 
-    CommandResponse execute(CommandRequest req);
+    CommandResponse execute(CommandRequest req) throws CommandException;
 }
