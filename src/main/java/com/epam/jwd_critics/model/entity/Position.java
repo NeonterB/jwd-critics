cb0926097b0910fb22b3121cdfea32219f3d4768
Neonter <barkan.makar@gmail.com>
2021-06-20 21:14:16 +0300

    Column renamed (position_id -> position)

--- src/main/java/com/epam/jwd_critics/model/entity/Position.java
+++ src/main/java/com/epam/jwd_critics/model/entity/Position.java
@@ -4,7 +4,7 @@ import com.epam.jwd_critics.exception.UnknownEntityException;
 
 import java.util.Arrays;
 
-@Column(name = "position_id")
+@Column(name = "position")
 public enum Position implements BaseEntity {
     DIRECTOR(1),
     ACTOR(2),
