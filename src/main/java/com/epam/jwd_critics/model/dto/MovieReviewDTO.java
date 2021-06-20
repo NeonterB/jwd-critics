9e4281cf0145d281029221be8a34e90e29ee2e9c
Neonter <barkan.makar@gmail.com>
2021-06-20 21:10:16 +0300

    DTOs

---
+++ src/main/java/com/epam/jwd_critics/model/dto/MovieReviewDTO.java
@@ -0,0 +1,57 @@
+package com.epam.jwd_critics.model.dto;
+
+import com.epam.jwd_critics.model.entity.MovieReview;
+
+public class MovieReviewDTO {
+    private Integer score;
+    private String text;
+    private String userImagePath;
+    private String userName;
+    private Integer userId;
+
+    public MovieReviewDTO(MovieReview review){
+        this.score = review.getScore();
+        this.text = review.getText();
+        this.userId = review.getUserId();
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getUserImagePath() {
+        return userImagePath;
+    }
+
+    public void setUserImagePath(String userImagePath) {
+        this.userImagePath = userImagePath;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
