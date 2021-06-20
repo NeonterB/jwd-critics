6fc74d7238834cc38fc728ae82869d9a1fbf6c5b
Neonter <barkan.makar@gmail.com>
2021-06-20 13:21:21 +0300

    new methods

--- src/main/java/com/epam/jwd_critics/model/dao/AbstractMovieReviewDao.java
+++ src/main/java/com/epam/jwd_critics/model/dao/AbstractMovieReviewDao.java
@@ -6,7 +6,11 @@ import com.epam.jwd_critics.model.entity.MovieReview;
 import java.util.List;
 
 public abstract class AbstractMovieReviewDao extends AbstractBaseDao<Integer, MovieReview> {
-    public abstract List<MovieReview> getMovieReviewsByMovieId(Integer movieId) throws DaoException;
+    public abstract List<MovieReview> getMovieReviewsByMovieId(Integer movieId, Integer begin, Integer end) throws DaoException;
 
-    public abstract List<MovieReview> getMovieReviewsByUserId(Integer userId) throws DaoException;
+    public abstract List<MovieReview> getMovieReviewsByUserId(Integer userId, Integer begin, Integer end) throws DaoException;
+
+    public abstract int getCountByMovieId(Integer movieId) throws DaoException;
+
+    public abstract int getCountByUserId(Integer userId) throws DaoException;
 }
