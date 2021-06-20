fdb7bfd7e2da62e3f3bada5575c73cf614393c16
Neonter <barkan.makar@gmail.com>
2021-06-20 21:13:56 +0300

    movie page

---
+++ src/main/java/com/epam/jwd_critics/controller/command/impl/common/OpenMoviePageCommand.java
@@ -0,0 +1,49 @@
+package com.epam.jwd_critics.controller.command.impl.common;
+
+import com.epam.jwd_critics.controller.command.Attribute;
+import com.epam.jwd_critics.controller.command.Command;
+import com.epam.jwd_critics.controller.command.CommandRequest;
+import com.epam.jwd_critics.controller.command.CommandResponse;
+import com.epam.jwd_critics.controller.command.Parameter;
+import com.epam.jwd_critics.controller.command.ServletDestination;
+import com.epam.jwd_critics.controller.command.TransferType;
+import com.epam.jwd_critics.exception.CommandException;
+import com.epam.jwd_critics.exception.ServiceException;
+import com.epam.jwd_critics.model.dto.MovieDTO;
+import com.epam.jwd_critics.model.entity.Movie;
+import com.epam.jwd_critics.model.service.MovieService;
+import com.epam.jwd_critics.model.service.impl.MovieServiceImpl;
+
+import java.util.Optional;
+
+public class OpenMoviePageCommand implements Command {
+    private final MovieService movieService = MovieServiceImpl.getInstance();
+
+    @Override
+    public CommandResponse execute(CommandRequest req) throws CommandException {
+        CommandResponse commandResult = new CommandResponse(ServletDestination.MOVIE, TransferType.FORWARD);
+        String movieIdStr = req.getParameter(Parameter.MOVIE_ID);
+        if (movieIdStr == null) {
+            req.setSessionAttribute(Attribute.GLOBAL_ERROR, "Empty movie id");
+            commandResult.setDestination(ServletDestination.MAIN);
+            commandResult.setTransferType(TransferType.REDIRECT);
+        } else {
+            try {
+                Optional<Movie> movie = movieService.getEntityById(Integer.valueOf(movieIdStr));
+                if (movie.isPresent()) {
+                    req.setSessionAttribute(Attribute.MOVIE, new MovieDTO(movie.get()));
+                    req.setSessionAttribute(Attribute.MOVIE_ID, movie.get().getId());
+                } else {
+                    req.setSessionAttribute(Attribute.GLOBAL_ERROR, "Movie does not exist");
+                    commandResult.setDestination(ServletDestination.MAIN);
+                    commandResult.setTransferType(TransferType.REDIRECT);
+                }
+            } catch (ServiceException e) {
+                req.setSessionAttribute(Attribute.SERVICE_ERROR, e.getMessage());
+                commandResult.setDestination(ServletDestination.MAIN);
+                commandResult.setTransferType(TransferType.REDIRECT);
+            }
+        }
+        return new GetMovieReviewsCommand().execute(req);
+    }
+}
