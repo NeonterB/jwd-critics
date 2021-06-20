44087ccdf7f41abdb07eec1a4791ea86005891b3
Neonter <barkan.makar@gmail.com>
2021-06-20 21:11:35 +0300

    post -> get

--- src/main/java/com/epam/jwd_critics/tag/TagUtil.java
+++ src/main/java/com/epam/jwd_critics/tag/TagUtil.java
@@ -1,21 +1,23 @@
 package com.epam.jwd_critics.tag;
 
+import com.epam.jwd_critics.controller.command.Parameter;
+
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.PageContext;
 import java.io.IOException;
 
 public class TagUtil {
-    static void paginate(PageContext pageContext, int pageCount, String commandName) throws JspException {
+    static void paginate(PageContext pageContext, int pageCount, String commandName, Parameter newPage) throws JspException {
         try {
             JspWriter writer = pageContext.getOut();
             String contextPath = pageContext.getServletContext().getContextPath();
-            writer.write("<form method=\"post\" action=\"" + contextPath + "/controller\">");
+            writer.write("<form method=\"get\" action=\"" + contextPath + "/controller\">");
             writer.write("<input type=\"hidden\" name=\"command\" value=\"" + commandName + "\"/>");
             writer.write("<nav>");
             writer.write("<ul class=\"pagination\">");
             for (int i = 0; i < pageCount; i++) {
-                createButton(writer, i + 1);
+                createButton(writer, i + 1, newPage.getName());
             }
             writer.write("</ul>");
             writer.write("</div>");
@@ -25,8 +27,8 @@ public class TagUtil {
             throw new JspException(e);
         }
     }
-    static void createButton(JspWriter writer, int pageNumber) throws IOException {
-        writer.write("<li class=\"page-item\"><button type=\"submit\" name=\"newPage\" ");
+    static void createButton(JspWriter writer, int pageNumber, String newPage) throws IOException {
+        writer.write("<li class=\"page-item\"><button type=\"submit\" name=\"" + newPage + "\" ");
         writer.write("value=\"" + pageNumber + "\" ");
         writer.write(">");
         writer.write(pageNumber + " </button></li>");
