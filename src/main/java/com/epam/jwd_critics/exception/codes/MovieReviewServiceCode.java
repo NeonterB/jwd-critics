73f3f8a2e9b984e1b6b74caf1517e6e0d482072f
Neonter <barkan.makar@gmail.com>
2021-06-21 17:59:52 +0300

    new code

--- src/main/java/com/epam/jwd_critics/exception/codes/MovieReviewServiceCode.java
+++ src/main/java/com/epam/jwd_critics/exception/codes/MovieReviewServiceCode.java
@@ -1,7 +1,8 @@
 package com.epam.jwd_critics.exception.codes;
 
 public enum MovieReviewServiceCode implements ExceptionCode {
-    REVIEW_DOES_NOT_EXIST("Movie review does not exist");
+    REVIEW_DOES_NOT_EXIST("Movie review does not exist"),
+    USER_ALREADY_LEFT_A_REVIEW("User already left a review");
     private final String msg;
 
     MovieReviewServiceCode(String msg) {
