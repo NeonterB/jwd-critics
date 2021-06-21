95a467793b42cae057bf36ac90a02d801d731a00
Neonter <barkan.makar@gmail.com>
2021-06-22 01:14:26 +0300

    new attrs

--- src/main/java/com/epam/jwd_critics/controller/command/Attribute.java
+++ src/main/java/com/epam/jwd_critics/controller/command/Attribute.java
@@ -10,13 +10,14 @@ public enum Attribute {
     MOVIES_TO_DISPLAY("allMoviesList"),
     MOVIE_COUNT("movieCount"),
     MOVIE("movie"),
+    REVIEWS_ON_MOVIE_PAGE("reviewsOnMoviePage"),
     USER_REVIEW("userReview"),
     REVIEWS_TO_DISPLAY("reviewsToDisplay"),
     REVIEW_COUNT("reviewCount"),
     VALIDATION_ERRORS("validationErrors"),
     SERVICE_ERROR("serviceError"),
     GLOBAL_ERROR("globalError"),
-    FRIENDLY_NOTIFICATION("friendlyNotification"),
+    SUCCESS_NOTIFICATION("successNotification"),
     REPORT_MESSAGE("reportMessage");
     private final String name;
 
