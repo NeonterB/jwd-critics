c76dc146390e15329bbbb7e2964f4898eae85793
Neonter <barkan.makar@gmail.com>
2021-06-21 17:19:18 +0300

    new command

--- src/main/java/com/epam/jwd_critics/controller/command/impl/user/CreateMovieReviewCommand.java
+++ src/main/java/com/epam/jwd_critics/controller/command/impl/user/CreateMovieReviewCommand.java
@@ -1,13 +1,50 @@
 package com.epam.jwd_critics.controller.command.impl.user;
 
+import com.epam.jwd_critics.controller.command.Attribute;
 import com.epam.jwd_critics.controller.command.Command;
 import com.epam.jwd_critics.controller.command.CommandRequest;
 import com.epam.jwd_critics.controller.command.CommandResponse;
-import com.epam.jwd_critics.exception.CommandException;
+import com.epam.jwd_critics.controller.command.Parameter;
+import com.epam.jwd_critics.controller.command.ServletDestination;
+import com.epam.jwd_critics.controller.command.TransferType;
+import com.epam.jwd_critics.controller.validation.ConstraintViolation;
+import com.epam.jwd_critics.controller.validation.MovieReviewValidator;
+import com.epam.jwd_critics.exception.ServiceException;
+import com.epam.jwd_critics.model.entity.MovieReview;
+import com.epam.jwd_critics.model.service.MovieReviewService;
+import com.epam.jwd_critics.model.service.impl.MovieReviewServiceImpl;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class CreateMovieReviewCommand implements Command {
+    private final MovieReviewService reviewService = MovieReviewServiceImpl.getInstance();
+
     @Override
-    public CommandResponse execute(CommandRequest req) throws CommandException {
-        return null;
+    public CommandResponse execute(CommandRequest req) {
+        CommandResponse commandResult = new CommandResponse(ServletDestination.MOVIE, TransferType.REDIRECT);
+        String reviewScore = req.getParameter(Parameter.MOVIE_REVIEW_SCORE);
+        String reviewText = req.getParameter(Parameter.MOVIE_REVIEW_TEXT);
+        String movieIdStr = req.getParameter(Parameter.MOVIE_ID);
+        String userIdStr = req.getParameter(Parameter.USER_ID);
+        if (reviewScore == null || reviewText == null || movieIdStr == null || userIdStr == null) {
+            req.setSessionAttribute(Attribute.VALIDATION_ERRORS, "Empty review fields");
+        } else {
+            MovieReviewValidator reviewValidator = new MovieReviewValidator();
+            Set<ConstraintViolation> violations = reviewValidator.validateReview(userIdStr, movieIdStr, reviewText, reviewScore);
+
+            if (violations.isEmpty()) {
+                try {
+                    reviewService.create(new MovieReview(reviewText, Integer.valueOf(reviewScore), Integer.valueOf(userIdStr), Integer.valueOf(movieIdStr)));
+                } catch (ServiceException e) {
+                    req.setSessionAttribute(Attribute.SERVICE_ERROR, e.getMessage());
+                }
+            } else {
+                req.setSessionAttribute(Attribute.VALIDATION_ERRORS, violations.stream()
+                        .map(ConstraintViolation::getMessage)
+                        .collect(Collectors.toList()));
+            }
+        }
+        return commandResult;
     }
 }
