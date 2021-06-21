ad855bae3b7d8fe92963ba2ee9482a66e907b8b7
Neonter <barkan.makar@gmail.com>
2021-06-21 17:16:18 +0300

    status check

--- src/main/java/com/epam/jwd_critics/controller/command/CommandInstance.java
+++ src/main/java/com/epam/jwd_critics/controller/command/CommandInstance.java
@@ -8,6 +8,7 @@ import com.epam.jwd_critics.controller.command.impl.guest.SignInCommand;
 import com.epam.jwd_critics.controller.command.impl.guest.RegisterCommand;
 import com.epam.jwd_critics.controller.command.impl.common.OpenSignInPageCommand;
 import com.epam.jwd_critics.controller.command.impl.common.OpenMainPageCommand;
+import com.epam.jwd_critics.controller.command.impl.user.CreateMovieReviewCommand;
 import com.epam.jwd_critics.controller.command.impl.user.SignOutCommand;
 import com.epam.jwd_critics.model.entity.Role;
 
@@ -21,20 +22,32 @@ public enum CommandInstance {
     OPEN_ALL_MOVIES(new OpenAllMoviesPageCommand(), Role.values()),
     OPEN_MOVIE(new OpenMoviePageCommand(), Role.values()),
     GET_MOVIE_REVIEWS(new GetMovieReviewsCommand(), Role.values()),
+    CREATE_MOVIE_REVIEW(new CreateMovieReviewCommand(), true, Role.USER, Role.ADMIN),
     CHANGE_LANGUAGE(new ChangeLocaleCommand(), Role.values()),
     SIGN_IN(new SignInCommand(), Role.GUEST),
     REGISTER(new RegisterCommand(), Role.GUEST),
     SIGN_OUT(new SignOutCommand(), Role.ADMIN, Role.USER);
     private final Command command;
     private final List<Role> allowedRoles = new LinkedList<>();
+    private boolean userMustBeActive = false;
 
     CommandInstance(Command command, Role ...roles) {
         this.command = command;
         this.allowedRoles.addAll(Arrays.asList(roles));
     }
 
+    CommandInstance(Command command, boolean mustBeActive, Role ...roles) {
+        this.command = command;
+        this.userMustBeActive = mustBeActive;
+        this.allowedRoles.addAll(Arrays.asList(roles));
+    }
+
     public boolean isRoleAllowed(Role role){
         return allowedRoles.contains(role);
+    }
+
+    public boolean isUserMustBeActive() {
+        return userMustBeActive;
     }
 
     public static Command commandOf(String commandName) {
