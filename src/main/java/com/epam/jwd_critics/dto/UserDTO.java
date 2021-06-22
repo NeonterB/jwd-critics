2ac04ff4adbdf80aa9d1f1f46033da3f9b0e952f
Neonter <barkan.makar@gmail.com>
2021-06-22 18:59:55 +0300

    added some fields

--- src/main/java/com/epam/jwd_critics/dto/UserDTO.java
+++ src/main/java/com/epam/jwd_critics/dto/UserDTO.java
@@ -1,18 +1,29 @@
 package com.epam.jwd_critics.dto;
 
+import com.epam.jwd_critics.entity.MovieReview;
 import com.epam.jwd_critics.entity.Role;
 import com.epam.jwd_critics.entity.Status;
 import com.epam.jwd_critics.entity.User;
+
+import java.util.List;
 
 public class UserDTO {
     private Integer id;
     private Role role;
     private Status status;
+    private String firstName;
+    private String lastName;
+    private String imagePath;
+
+    private List<MovieReview> reviews;
 
     public UserDTO(User user){
         this.id = user.getId();
         this.role = user.getRole();
         this.status = user.getStatus();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.imagePath = user.getImagePath();
     }
 
     public Integer getId() {
@@ -37,5 +48,41 @@ public class UserDTO {
 
     public void setStatus(Status status) {
         this.status = status;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getName(){
+        return firstName + " " + lastName;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public List<MovieReview> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<MovieReview> reviews) {
+        this.reviews = reviews;
     }
 }
