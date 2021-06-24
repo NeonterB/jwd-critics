1de94bc9ac80cd68ed682c321f2ed62eebdf9bf8
Neonter <barkan.makar@gmail.com>
2021-06-24 11:54:20 +0300

    PictureServlet created

---
+++ src/main/java/com/epam/jwd_critics/controller/PictureServlet.java
@@ -0,0 +1,30 @@
+package com.epam.jwd_critics.controller;
+
+import com.epam.jwd_critics.controller.command.Parameter;
+import com.epam.jwd_critics.util.PropertiesLoaderUtil;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@WebServlet("/picture")
+public class PictureServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String pictureName = request.getParameter(Parameter.CURRENT_PICTURE.getName());
+            String assetsDir = PropertiesLoaderUtil.getApplicationProperties().getAssetsDir();
+            byte[] image = Files.readAllBytes(Paths.get(assetsDir + '/' + pictureName));
+            response.setHeader("Content-Disposition", "inline filename=\"" + pictureName + "\"");
+            response.setContentType(getServletContext().getMimeType(pictureName));
+            response.setContentLength(image.length);
+            response.getOutputStream().write(image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
