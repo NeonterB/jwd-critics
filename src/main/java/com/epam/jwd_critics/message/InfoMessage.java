8fc71fdbcfca498a8daf5d13a10b8b90efee6e9e
Neonter <barkan.makar@gmail.com>
2021-06-25 01:47:01 +0300

    activate_email msg

--- src/main/java/com/epam/jwd_critics/message/InfoMessage.java
+++ src/main/java/com/epam/jwd_critics/message/InfoMessage.java
@@ -3,4 +3,5 @@ package com.epam.jwd_critics.message;
 public class InfoMessage {
     public static final String NO_MOVIES = "No movies here yet";
     public static final String INACTIVE_USER = "That user is inactive";
+    public static final String ACTIVATE_EMAIL = "We have send an activation link to your email";
 }
