fcbb33e021d6f6f601126644c75e004c58885b60
Neonter <barkan.makar@gmail.com>
2021-06-29 18:20:45 +0300

    Renamed method

--- src/main/java/com/epam/jwd_critics/dao/AbstractMovieDao.java
+++ src/main/java/com/epam/jwd_critics/dao/AbstractMovieDao.java
@@ -1,15 +1,15 @@
 package com.epam.jwd_critics.dao;
 
-import com.epam.jwd_critics.exception.DaoException;
 import com.epam.jwd_critics.entity.Genre;
 import com.epam.jwd_critics.entity.Movie;
 import com.epam.jwd_critics.entity.Position;
+import com.epam.jwd_critics.exception.DaoException;
 
 import java.util.List;
 import java.util.Map;
 
 public abstract class AbstractMovieDao extends AbstractBaseDao<Integer, Movie> {
-    public abstract Map<Movie, List<Position>> getMoviesByCelebrityId(Integer celebrityId) throws DaoException;
+    public abstract Map<Movie, List<Position>> getJobsByCelebrityId(Integer celebrityId) throws DaoException;
 
     public abstract List<Genre> getMovieGenresById(Integer movieId) throws DaoException;
 
