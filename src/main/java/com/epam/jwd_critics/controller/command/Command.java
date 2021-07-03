c385db1ab9e27143cdf2a7253a72fada27e7cd65
Neonter <barkan.makar@gmail.com>
2021-07-03 10:37:34 +0300

    commandOf->getCommandBeName

--- src/main/java/com/epam/jwd_critics/controller/command/Command.java
+++ src/main/java/com/epam/jwd_critics/controller/command/Command.java
@@ -5,7 +5,7 @@ import com.epam.jwd_critics.exception.CommandException;
 @FunctionalInterface
 public interface Command {
     static Command of(String commandName) {
-        return CommandInstance.commandOf(commandName);
+        return CommandInstance.getCommandByName(commandName);
     }
 
     CommandResponse execute(CommandRequest req) throws CommandException;
